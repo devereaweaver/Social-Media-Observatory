2483dda5e8dfede5d28d8c37755ffc2f237ee6a6
daw <deveredev@gmail.com>
2025-01-19 15:15:53 -0500

    added gitignore

--- get_channel_info_poc.py
+++ get_channel_info_poc.py
@@ -1,12 +1,9 @@
 # Proof of concept to prove we can retrieve channel metadata from the Telegram API
+# Note: This POC code is a sequential version, may consider using async version for production
+
 
 from telethon.sync import TelegramClient
 from telethon import functions
-
-# Configuration data
-app_name =  "D's SMOP"
-api_id = 27996747
-api_hash = "6cfd4e4453cb1b91d8c5590d9d20c6f0"
 
 # Input data
 channel_names = ["rybar", "mig41"]
@@ -18,3 +15,4 @@ with TelegramClient(app_name, api_id, api_hash) as client:
 
     if channel_object is not None:
         print(channel_object.to_dict())
+    
