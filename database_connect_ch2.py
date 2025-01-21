87ed7fa6bf3a8abc520d22ff08d86bc2976211ab
devereaweaver <devereaweaver@icloud.com>
2025-01-20 19:12:37 -0500

    Added try-except block to database_connect_ch2.py to test for connection to local postgres instance

--- database_connect_ch2.py
+++ database_connect_ch2.py
@@ -2,12 +2,19 @@
 from config import config
 import sqlalchemy as sa
 
-engine = sa.create_engine(
-    f"postgresql://"
-    f"{config['telegram-db']['user']}:"
-    f"{config['telegram-db']['password']}:"
-    f"@{config['telegram-db']['host']}:"
-    f"{config['telegram-db']['port']}/"
-    f"{config['telegram-db']['dbname']}",
-    echo=True,
-)
+try:
+    engine = sa.create_engine(
+        f"postgresql://"
+        f"{config['telegram-db']['user']}:"
+        f"{config['telegram-db']['password']}:"
+        f"@{config['telegram-db']['host']}:"
+        f"{config['telegram-db']['port']}/"
+        f"{config['telegram-db']['dbname']}",
+        echo=True,
+    )
+
+    connection = engine.connect()
+    print("Connection to PostgreSQL DB successful")
+
+except Exception as e:
+    print("Connection to PostgreSQL DB failed")
