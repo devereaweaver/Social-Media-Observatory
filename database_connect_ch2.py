85d4a55ff979c9b064a4b53d5d80fa22aa1a112a
devereaweaver <devereaweaver@icloud.com>
2025-01-21 10:23:26 -0500

    Added logic.py to extract data from the returned channel object from the Telegram API JSON response. Added function to insert channel metadata into the corresponding Postgres table and database. Updated get_channel_info_poc.py to use the newly created functions from the other modules and fixed with block to iterate over a list of channel names instead of only doing one channel name at a time.

--- database_connect_ch2.py
+++ database_connect_ch2.py
@@ -1,20 +1,60 @@
-# Connect to the PostgreSQL database server
+# This script is used to create a schema for the database that will store the metadata
+# of the Telegram channels
 from config import config
 import sqlalchemy as sa
+from datetime import datetime
 
-try:
-    engine = sa.create_engine(
-        f"postgresql://"
-        f"{config['telegram-db']['user']}:"
-        f"{config['telegram-db']['password']}:"
-        f"@{config['telegram-db']['host']}:"
-        f"{config['telegram-db']['port']}/"
-        f"{config['telegram-db']['dbname']}",
-        echo=True,
-    )
 
-    connection = engine.connect()
-    print("Connection to PostgreSQL DB successful")
+def insert_data_into_channel_metadata_table(records: list[dict]):
+    """
+    inserts data into the channel_metadata table
+    """
+    # Create a prepared SQL statement using the channel_metadata table object
+    # this is instantiated below
+    stmt = sa.insert(channel_metadata).values(records)
 
-except Exception as e:
-    print("Connection to PostgreSQL DB failed")
+    # Initiate a connection with the database
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()  # Commit the transaction to the database
+
+
+# Attempt to connect to the PostgreSQL database server w/credentials from config file
+engine = sa.create_engine(
+    f"postgresql://"
+    f"{config['telegram-db']['user']}:"
+    f"{config['telegram-db']['password']}:"
+    f"@{config['telegram-db']['host']}:"
+    f"{config['telegram-db']['port']}/"
+    f"{config['telegram-db']['dbname']}",
+    echo=True,
+)
+
+connection = engine.connect()
+print("Connection to PostgreSQL DB successful")
+
+# Create a metadata object that acts as a container for database schema definitions
+# (e.g. tables, columns, indexes, etc.)
+meta = sa.MetaData()
+
+# TODO: Place this code into a separate function after making sure the code
+# is working properly for production
+# Create a Table object that represents the channel_metadata table and associated metadata
+channel_metadata = sa.Table(
+    "channel_metadata",
+    meta,
+    sa.Column("channel_id", sa.types.BIGINT, primary_key=True),
+    sa.Column("channel_name", sa.types.TEXT, unique=True),
+    sa.Column("channel_title", sa.types.TEXT, default=None),
+    sa.Column("channel_birthdate", sa.types.DateTime(timezone=True)),
+    sa.Column("channel_bio", sa.types.TEXT, default=None),
+    sa.Column("num_subscribers", sa.types.INTEGER, default=None),
+    sa.Column("data_source", sa.types.TEXT, default="telegram-api"),
+    sa.Column(
+        "checkup_time", sa.types.DateTime(timezone=True), default=datetime.utcnow
+    ),
+    sa.Column("api_response", sa.types.JSON, nullable=False),
+)
+
+# Create new tables only if they don't already exist
+meta.create_all(engine)
--- get_channel_info_poc.py
+++ get_channel_info_poc.py
@@ -4,15 +4,30 @@
 from telethon.sync import TelegramClient
 from telethon import functions
 from config import app_name, api_id, api_hash
+from database_connect_ch2 import insert_data_into_channel_metadata_table
+from logic import extract_data_dictionary_from_channel_object
 
 # Input data
 channel_names = ["rybar", "mig41"]
+#channel_names = ["rybar"]
+
+
+# Running list of records
+records = []
 
 # Retrieve channel metadata from Telegram API
 with TelegramClient(app_name, api_id, api_hash) as client:
     for channel_name in channel_names:
         channel_object = client(functions.channels.GetFullChannelRequest(channel=channel_name))
 
-    if channel_object is not None:
-        print(channel_object.to_dict())
-    
+        # Store non-empty returned objects in records list and extract relevant fields 
+        # to Postgres table columns
+        if channel_object is not None:
+            records.append(extract_data_dictionary_from_channel_object(channel_object))
+
+# Insert the data into our Postgres table
+if len(records) > 0:
+    insert_data_into_channel_metadata_table(records) # pass list of records to function
+    print("Data inserted successfully")
+else:
+    print("Error: Insertion failed")
