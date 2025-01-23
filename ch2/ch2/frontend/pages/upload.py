03f1d498f408c5183f768b0acd8931b8f0a55553
devereaweaver <devereaweaver@icloud.com>
2025-01-23 17:01:35 -0500

    Added Dash component to allow users to drag-and-drop or search their filesystem to upload csv files

--- ch2/ch2/frontend/pages/upload.py
+++ ch2/ch2/frontend/pages/upload.py
@@ -1,8 +1,28 @@
 # Upload Telegram handles 
-from dash import html 
+from dash import html , dcc
 
 layout = html.Div(
     [
-        html.P("This page is not ready yet :/")
+        # Add upload component
+        dcc.Upload(
+            id = "drag-and-drop",
+            children=html.Div(
+                [
+                    f"Drag and drop your CSV here, or ",
+                    html.A("click here to browse your file system"),
+                ]
+            ),
+            style={
+            "width": "99%",
+            "height": "60px",
+            "lineHeight": "60px",
+            "borderWidth": "1px",
+            "borderStyle": "dashed",           
+            "borderRadius": "5px",
+            "textAlign": "center",
+            "margin": "10px",
+            },
+            multiple=False,
+        )
     ]
 )
