ba297342c3e9dba80f0170fef97e9dc38c5067f8
devereaweaver <devereaweaver@icloud.com>
2025-01-23 09:24:00 -0500

    restarted project with POC script

--- config.py
+++ config.py
@@ -1,21 +1,20 @@
+# config.py - load config data from file in user's home directory
 import configparser
 import os 
 import platform
 
-# Point to the config file
-HOME_DIR = (
-    os.environ["USERPROFILE"] if platform.system() == "Windows"
-    else os.environ["HOME"]
-)
+# Point to config file 
+HOME_DIR = os.environ["USERPROFILE"] if platform.system() == "Windows" else os.environ["HOME"]
+
 config_file_full_path = os.path.join(HOME_DIR, "smo_config.cfg")
 
-# Instantiate a config manager object
+# Instantiate config manager object
 config = configparser.ConfigParser()
 
-# Read in the config file
+# Load config data from file
 config.read(config_file_full_path)
 
-# Extract the config data into Python variables (optional)
-app_name=config['telegram-credentials']['app-name']
-api_id=config['telegram-credentials']['api-id']
-api_hash=config['telegram-credentials']['api-hash']
+# Extract config data into Python variables (optional)
+app_name = config['telegram-credentials']['app-name']
+api_id = config['telegram-credentials']['api-id']
+api_hash = config['telegram-credentials']['api-hash']
--- get_channel_info_poc.py
+++ get_channel_info_poc.py
@@ -1,33 +1,15 @@
-# Proof of concept to prove we can retrieve channel metadata from the Telegram API
-# Note: This POC code is a sequential version, may consider using async version for production
-
+# POC script to get channel metadata from Telegram API 
 from telethon.sync import TelegramClient
 from telethon import functions
 from config import app_name, api_id, api_hash
-from database_connect_ch2 import insert_data_into_channel_metadata_table
-from logic import extract_data_dictionary_from_channel_object
 
-# Input data
+# Hardcode input data
 channel_names = ["rybar", "mig41"]
-#channel_names = ["rybar"]
 
-
-# Running list of records
-records = []
-
-# Retrieve channel metadata from Telegram API
+# Retrieve channel metadata 
 with TelegramClient(app_name, api_id, api_hash) as client:
     for channel_name in channel_names:
         channel_object = client(functions.channels.GetFullChannelRequest(channel=channel_name))
-
-        # Store non-empty returned objects in records list and extract relevant fields 
-        # to Postgres table columns
-        if channel_object is not None:
-            records.append(extract_data_dictionary_from_channel_object(channel_object))
-
-# Insert the data into our Postgres table
-if len(records) > 0:
-    insert_data_into_channel_metadata_table(records) # pass list of records to function
-    print("Data inserted successfully")
-else:
-    print("Error: Insertion failed")
+    
+    if channel_object is not None:
+        print(channel_object.to_dict())
