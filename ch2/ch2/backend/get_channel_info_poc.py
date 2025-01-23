373d446eb0514cb743551ecf4844edecbf12df76
devereaweaver <devereaweaver@icloud.com>
2025-01-23 11:35:22 -0500

    Added runner code for POC code to make sure that the telegram API connection code is still working

--- ch2/ch2/backend/get_channel_info_poc.py
+++ ch2/ch2/backend/get_channel_info_poc.py
@@ -1,28 +1,29 @@
 # POC script to get channel metadata from Telegram API
 from telethon.sync import TelegramClient
 from telethon import functions
-from config import app_name, api_id, api_hash
-from db_ch2 import insert_data_into_channel_metadata_table
-from logic_ch2 import extract_data_dictionary_from_channel_object
+from ..config import app_name, api_id, api_hash
+from ..utilities.db_ch2 import insert_data_into_channel_metadata_table
+from ..utilities.logic_ch2 import extract_data_dictionary_from_channel_object
 
-# Hardcode input data
-channel_names = ["rybar", "mig41"]
+def run():
+    # Hardcode input data
+    channel_names = ["rybar", "mig41"]
 
-# Empty list to store records pulled from API 
-records = []
+    # Empty list to store records pulled from API 
+    records = []
 
-# Retrieve channel metadata
-with TelegramClient(app_name, api_id, api_hash) as client:
-    for channel_name in channel_names:
-        channel_object = client(
-            functions.channels.GetFullChannelRequest(channel=channel_name)
-        )
+    # Retrieve channel metadata
+    with TelegramClient(app_name, api_id, api_hash) as client:
+        for channel_name in channel_names:
+            channel_object = client(
+                functions.channels.GetFullChannelRequest(channel=channel_name)
+            )
 
-        # Extract interesting data from returned API object into format
-        # that matches the database schema, then add each one to the records list
-        if channel_object is not None:
-            records.append(extract_data_dictionary_from_channel_object(channel_object))
+            # Extract interesting data from returned API object into format
+            # that matches the database schema, then add each one to the records list
+            if channel_object is not None:
+                records.append(extract_data_dictionary_from_channel_object(channel_object))
 
-# Store data into table
-if len(records) > 0:
-    insert_data_into_channel_metadata_table(records)
+    # Store data into table
+    if len(records) > 0:
+        insert_data_into_channel_metadata_table(records)
---
+++ ch2/run_poc.py
@@ -0,0 +1,3 @@
+from ch2.backend.get_channel_info_poc import run
+
+run()
